9f5f7e70f19a88397e24f2776750bd4409b0685c
unknown <arslanyounas344@gmail.com>
2024-04-26 11:59:01 -0700

    working on landing page functionality

--- src/components/search/searchinput.jsx
+++ src/components/search/searchinput.jsx
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
 import { CiSearch } from "react-icons/ci";
+import DataContext from "../../contexts/dataContext.jsx";
+import React, { useContext,useState } from "react";
+
 
 const SearchInput = () => {
   const [isInputFocused, setIsInputFocused] = useState(false);
   const [searchedJob, setSearchedJob] = useState("");
+  const data = useContext(DataContext);
+  console.log(data.post_name,"data on serachpage");
+
+
+  const handleSearch = () => { 
+    const filteredData = data.filter((filteredItem) => {
+    console.log(filteredItem,"filteredItem")
+      return filteredItem.post_name
+        .toLowerCase()
+        .includes(searchedJob.toLowerCase());
+    })
+    console.log("i am working ok ok",filteredData)
+  }
 
   const handleSearchedJob = (e) => {
     setSearchedJob(e.target.value);
@@ -25,16 +40,20 @@ const SearchInput = () => {
         }`}
       >
         <input
-          className="border-none outline-none"
+          className="border-none outline-none bg-gray-100"
           placeholder="Search"
           type="text"
           onFocus={handleOnFocus}
           onBlur={handleOnBlur}
-            value={searchedJob}
-            onChange={handleSearchedJob}
+          value={searchedJob}
+          onChange={handleSearchedJob}
             // still i have to manage the search functionality
         />
+
+        <button onClick={handleSearch}> 
         <CiSearch />
+          
+           </button>
       </div>
     </>
   );
