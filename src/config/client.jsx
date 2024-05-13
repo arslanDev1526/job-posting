fb6897446b59556133977219697fb4adeecb5e6c
unknown <arslanyounas344@gmail.com>
2024-05-13 11:52:09 -0700

    add env variables to the varcel

--- src/config/client.jsx
+++ src/config/client.jsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_PROJECT_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_PROJECT_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.meta.env.NEXT_PUBLIC_SUPABASE_PROJECT_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
