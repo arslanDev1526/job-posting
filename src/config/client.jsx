f2ebdf3b1def949831e8afe358d4af6947b40167
unknown <arslanyounas344@gmail.com>
2024-05-13 12:48:55 -0700

    stuck on same issuee

--- src/config/client.jsx
+++ src/config/client.jsx
@@ -1,8 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl =  new URL(import.meta.env.VITE_SUPABASE_URL)
-const supabaseKey = import.meta.env.VITE_SUPABASE_PROJECT_KEY
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_PROJECT_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default supabase;
